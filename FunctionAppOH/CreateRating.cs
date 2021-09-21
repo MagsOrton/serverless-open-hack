fd9d524fe8bcc93851e3006a27ef0b03c1e3537d
Margaret Orton <maorton@microsoft.com>
2021-09-21 13:22:58 +0100

    added create rating function

--- FunctionAppOH/CreateRating.cs
+++ FunctionAppOH/CreateRating.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace FunctionAppOH
 {
@@ -16,21 +17,49 @@ namespace FunctionAppOH
         [FunctionName("CreateRating")]
         public static async Task<IActionResult> CreateRating(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:  "%databaseName%", collectionName: "%collectionName%",
+            ConnectionStringSetting = "CosmosDbConnectionString"
+            )]IAsyncCollector<dynamic> documentsOut,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string payload  = req.Query["payload"];
-
+           
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            payload = payload  ?? data?.payload ;
+            
+            if (data != null) {
+                string userId = data.userId;
+                string productId = data.productId;
+                string locationName = data.locationName;
+                int rating = data.rating;
+                string userNotes = data.userNotes;
 
-            string responseMessage = string.IsNullOrEmpty(payload )
-                ? "This HTTP triggered function executed successfully. Pass a payload  in the query string or in the request body for a personalized response."
-                : $"The product name for your product id {payload }";
+                if (true /*add check for valid data*/) {
+                    try
+                    {
+                        await documentsOut.AddAsync(new
+                        {
+                            id = System.Guid.NewGuid().ToString(),
+                            userId = userId,
+                            productId = productId,
+                            timestamp = DateTime.UtcNow,
+                            locationName = locationName,
+                            rating = rating,
+                            userNotes = userNotes
+                        });
+                        return new OkObjectResult("New rating was inserted successfully");
+                    }
+                    catch(Exception ex)
+                    {
+                        log.LogError(ex.Message);
+                        return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+                    }
+                   
+                }
+            }
 
-            return new OkObjectResult(responseMessage);
+            return new StatusCodeResult((int)HttpStatusCode.BadRequest);
         }
-}
-}
+    }
+  }
